3755bfb84836c56e2c61526fe8bc1bbd5a9917c0
Ryan Welton <ryan.g.welton@gmail.com>
2015-03-18 18:16:02 -0400

    Update apktool to latest version and corresponding build flags

--- src/drozer/agent/manifest.py
+++ src/drozer/agent/manifest.py
@@ -1,20 +1,20 @@
 from xml.etree import ElementTree as xml
 
 class Endpoint(object):
-    
+
     def __init__(self, path):
         self.__path = path
-        
+
         lines = open(self.__path).read().split("\n")
         data = dict(map(lambda l: l.split(":"), filter(lambda l: l.find(":") > -1, lines)))
-        
+
         self.host = data['host']
         self.password = data['password']
         self.port = int(data['port'])
         self.ssl = data['ssl'].startswith("t")
         self.ts_password = data['ts_password']
         self.ts_path = data['ts_path']
-    
+
     def put_server(self, server):
         if isinstance(server, tuple):
             self.host, self.port = server
@@ -23,7 +23,7 @@ class Endpoint(object):
                 self.host, self.port = server.split(":")
             else:
                 self.host = server
-    
+
     def write(self):
         h = open(self.__path, 'w')
 
@@ -38,31 +38,27 @@ class Endpoint(object):
 
 
 class Manifest(object):
-    
+
     def __init__(self, path):
         self.__path = path
         self.__doc = xml.fromstring(file(self.__path).read())
-        
-    
+
+
     def add_permission(self, name):
         node = xml.Element('uses-permission')
         node.attrib["ns0:name"] = name
-        
+
         self.__doc.insert(len(self.__doc.getchildren()) - 1, node)
 
     def define_permission(self, name, protectionLevel):
         node = xml.Element('permission')
         node.attrib["ns0:name"] = name
         node.attrib["ns0:protectionLevel"] = protectionLevel
-        
-        self.__doc.insert(len(self.__doc.getchildren()) - 1, node)
-        
+
+
     def permissions(self):
         return self.__doc.findall('uses-permission')
 
     def write(self):
         xml.ElementTree(self.__doc).write(self.__path)
 
-    def version(self):
-        return self.__doc.attrib['{http://schemas.android.com/apk/res/android}versionName']
-        
